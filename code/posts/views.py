9f2ab2be45f5d27672e9c4951095172bfa7c3d2a
talgat.abdraimov <talgat.abdraimov@zimran.io>
2025-01-27 15:46:12 +0500

    added templates, bootstrap

--- code/posts/views.py
+++ code/posts/views.py
@@ -1,29 +1,28 @@
-from datetime import datetime
-
 from django.shortcuts import render
 
 
+def about(request):
+    return render(request, 'posts/about.html')
+
+
+articles_list = [
+    {'id': 1, 'title': 'First article', 'content': 'This is the first article content'},
+    {'id': 2, 'title': 'Second article', 'content': 'This is the second article content'},
+    {'id': 3, 'title': 'Third article', 'content': 'This is the third article content'},
+]
+
+
+def articles(request):
+    return render(request, 'posts/article-list.html', context={'articles': articles_list})
+
+
+def article_detail(request, article_id: int):
+    for article in articles_list:
+        if article['id'] == article_id:
+            return render(request, 'posts/article-detail.html', context={'article': article})
+
+    return render(request, 'posts/article-detail.html')
+
+
 def index(request):
-    published_at = datetime.now()
-
-    author = {'first_name': 'John', 'l': 'Doe'}
-
-    posts = [
-        {
-            'title': 'First post',
-            'text': 'This is my',
-        },
-        {
-            'title': 'Second post',
-            'text': 'This is my second post',
-        },
-        {
-            'title': 'Third post',
-            'text': 'This is my third post',
-        },
-    ]
-    return render(
-        request,
-        'posts/posts-list.html',
-        context={'author': author, 'publish_date': published_at, 'posts': posts},
-    )
+    return render(request, 'base.html', context={'name': 'John Doe'})
