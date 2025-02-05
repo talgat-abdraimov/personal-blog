41684397978ddff53f5df0abbde217ea00ad12ed
talgat.abdraimov <talgat.abdraimov@zimran.io>
2025-02-05 18:54:07 +0500

    added class based views

--- code/posts/models.py
+++ code/posts/models.py
@@ -43,3 +43,19 @@ class Category(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')  # INT
+
+    content = models.TextField(verbose_name=_('Контент'))  # TEXT
+
+    created_at = models.DateTimeField(auto_now_add=True)  # DATETIME
+    updated_at = models.DateTimeField(auto_now=True)  # DATETIME
+
+    class Meta:
+        verbose_name = _('Комментарий')
+        verbose_name_plural = _('Комментарии')
+
+    def __str__(self):
+        return f'{self.post}'
--- code/posts/urls.py
+++ code/posts/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
+from django.views.generic import TemplateView
 
-from .views import about, article_detail, articles, index
+from .views import ArticleView, ContactView
 
 urlpatterns = [
-    path('', index, name='index_page'),
-    path('articles/', articles, name='articles_page'),
-    path('articles/<str:slug>/', article_detail, name='article_detail'),
-    path('about-us/', about, name='about_page'),
+    path('', TemplateView.as_view(template_name='base.html'), name='index_page'),
+    path('articles/', ArticleView.as_view(), name='articles_page'),
+    path('articles/<str:slug>/', ArticleView.as_view(), name='article_detail'),
+    path('about-us/', TemplateView.as_view(template_name='posts/about.html'), name='about_page'),
+    path('contact/', ContactView.as_view(), name='contact_page'),
 ]
