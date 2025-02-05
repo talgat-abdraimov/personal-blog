a1713c03115d0aa34126657c8447eb4294359ccf
talgat.abdraimov <talgat.abdraimov@zimran.io>
2025-02-05 19:55:57 +0500

    added listview, createview and detailview

--- code/posts/urls.py
+++ code/posts/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
 from django.views.generic import TemplateView
 
-from .views import ArticleView, ContactView
+from .views import ArticleCreateView, ArticleDetailView, ArticleListView, ContactView
 
 urlpatterns = [
     path('', TemplateView.as_view(template_name='base.html'), name='index_page'),
-    path('articles/', ArticleView.as_view(), name='articles_page'),
-    path('articles/<str:slug>/', ArticleView.as_view(), name='article_detail'),
+    path('articles/', ArticleListView.as_view(), name='articles_page'),
+    path('articles/create/', ArticleCreateView.as_view(), name='article_create'),
+    path('articles/<str:human_readable_title>/', ArticleDetailView.as_view(), name='article_detail'),
     path('about-us/', TemplateView.as_view(template_name='posts/about.html'), name='about_page'),
     path('contact/', ContactView.as_view(), name='contact_page'),
 ]
